26a43927a6e3768f4d10104b1165aa93bd5305dc
Matheus Rocha <matheusrochacamarg@gmail.com>
2021-11-20 00:40:35 -0300

    database: creating the user table

--- app/sqls/001_inicial.sql
+++ app/sqls/001_inicial.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE php_drive COLLATE 'utf8_unicode_ci';
+
+CREATE TABLE usuarios (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  profilePhoto VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+ENGINE = InnoDB;
